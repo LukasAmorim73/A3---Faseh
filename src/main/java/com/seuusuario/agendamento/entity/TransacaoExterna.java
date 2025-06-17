83d063f8616f1917403bc31e7bcc089b5994f705
LuKas-73 <amorimlukasblu@gmail.com>
2025-06-17 18:38:53 -0300

    Update TransacaoExterna.java

--- src/main/java/com/seuusuario/agendamento/entity/TransacaoExterna.java
+++ src/main/java/com/seuusuario/agendamento/entity/TransacaoExterna.java
@@ -1,21 +1,29 @@
 package com.seuusuario.agendamento.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class TransacaoExterna {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idTransacao;
-    private String canal;
-    @Column(columnDefinition = "TEXT")
-    private String mensagem;
-    private LocalDateTime dataRecebimento;
-    private Long idCliente;
+    private int id;
+    private int clienteId;
+    private double valor;
+    private String tipo;
+
+    public TransacaoExterna() {}
+
+    public TransacaoExterna(int id, int clienteId, double valor, String tipo) {
+        this.id = id;
+        this.clienteId = clienteId;
+        this.valor = valor;
+        this.tipo = tipo;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getClienteId() { return clienteId; }
+    public void setClienteId(int clienteId) { this.clienteId = clienteId; }
+
+    public double getValor() { return valor; }
+    public void setValor(double valor) { this.valor = valor; }
+
+    public String getTipo() { return tipo; }
+    public void setTipo(String tipo) { this.tipo = tipo; }
 }
