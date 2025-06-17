7599416bbd2c5a559385ed5d06f5b0ee0ba06323
LuKas-73 <amorimlukasblu@gmail.com>
2025-06-17 18:41:34 -0300

    Update ProfissionalRepository.java

--- src/main/java/com/seuusuario/agendamento/repository/ProfissionalRepository.java
+++ src/main/java/com/seuusuario/agendamento/repository/ProfissionalRepository.java
@@ -1,9 +1,25 @@
-package com.seuusuario.agendamento.repository;
+package com.seuusuario.agendamento.dao;
 
+import com.seuusuario.agendamento.ConexaoComBancoDeDados;
 import com.seuusuario.agendamento.entity.Profissional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ProfissionalRepository extends JpaRepository<Profissional, Long> {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ProfissionalDAO {
+    public void cadastrarProfissional(Profissional profissional) {
+        String sql = "INSERT INTO profissional (nome, especialidade) VALUES (?, ?)";
+
+        try (Connection conn = ConexaoComBancoDeDados.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, profissional.getNome());
+            stmt.setString(2, profissional.getEspecialidade());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
