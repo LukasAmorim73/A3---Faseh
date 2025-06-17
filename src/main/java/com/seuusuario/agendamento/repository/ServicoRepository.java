e83ebcc2f13ec28e956cc6757d829cb478f1dfc3
LuKas-73 <amorimlukasblu@gmail.com>
2025-06-17 18:42:25 -0300

    Update ServicoRepository.java

--- src/main/java/com/seuusuario/agendamento/repository/ServicoRepository.java
+++ src/main/java/com/seuusuario/agendamento/repository/ServicoRepository.java
@@ -1,9 +1,25 @@
-package com.seuusuario.agendamento.repository;
+package com.seuusuario.agendamento.dao;
 
+import com.seuusuario.agendamento.ConexaoComBancoDeDados;
 import com.seuusuario.agendamento.entity.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ServicoRepository extends JpaRepository<Servico, Long> {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ServicoRepository {
+    public void cadastrarServico(Servico servico) {
+        String sql = "INSERT INTO servico (nome, preco) VALUES (?, ?)";
+
+        try (Connection conn = ConexaoComBancoDeDados.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, servico.getNome());
+            stmt.setDouble(2, servico.getPreco());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
