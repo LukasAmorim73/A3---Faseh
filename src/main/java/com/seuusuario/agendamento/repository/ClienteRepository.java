04592bce7a2d0a3cb2335597a56765af1f2e012a
LuKas-73 <amorimlukasblu@gmail.com>
2025-06-17 18:41:02 -0300

    Update ClienteRepository.java

--- src/main/java/com/seuusuario/agendamento/repository/ClienteRepository.java
+++ src/main/java/com/seuusuario/agendamento/repository/ClienteRepository.java
@@ -1,9 +1,26 @@
-package com.seuusuario.agendamento.repository;
+package com.seuusuario.agendamento.dao;
 
+import com.seuusuario.agendamento.ConexaoComBancoDeDados;
 import com.seuusuario.agendamento.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ClienteRepository {
+    public void cadastrarCliente(Cliente cliente) {
+        String sql = "INSERT INTO cliente (nome, telefone, email) VALUES (?, ?, ?)";
+
+        try (Connection conn = ConexaoComBancoDeDados.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setString(1, cliente.getNome());
+            stmt.setString(2, cliente.getTelefone());
+            stmt.setString(3, cliente.getEmail());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
