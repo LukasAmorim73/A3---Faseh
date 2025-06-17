39fc694ed9bd8806c0ab5b96742f6a3df90c0978
LuKas-73 <amorimlukasblu@gmail.com>
2025-06-17 18:44:51 -0300

    Update TransacaoExternaRepository.java

--- src/main/java/com/seuusuario/agendamento/repository/TransacaoExternaRepository.java
+++ src/main/java/com/seuusuario/agendamento/repository/TransacaoExternaRepository.java
@@ -1,9 +1,26 @@
-package com.seuusuario.agendamento.repository;
+package com.seuusuario.agendamento.dao;
 
+import com.seuusuario.agendamento.ConexaoComBancoDeDados;
 import com.seuusuario.agendamento.entity.TransacaoExterna;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface TransacaoExternaRepository extends JpaRepository<TransacaoExterna, Long> {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class TransacaoExternaRepository {
+    public void cadastrarTransacao(TransacaoExterna transacao) {
+        String sql = "INSERT INTO transacao_externa (cliente_id, valor, tipo) VALUES (?, ?, ?)";
+
+        try (Connection conn = ConexaoComBancoDeDados.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, transacao.getClienteId());
+            stmt.setDouble(2, transacao.getValor());
+            stmt.setString(3, transacao.getTipo());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
