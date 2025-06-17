f34b5f05e4268db4954af95c455288f51cad8d42
LuKas-73 <amorimlukasblu@gmail.com>
2025-06-17 18:36:14 -0300

    Update Cliente.java

--- src/main/java/com/seuusuario/agendamento/entity/Cliente.java
+++ src/main/java/com/seuusuario/agendamento/entity/Cliente.java
@@ -1,21 +1,29 @@
 package com.seuusuario.agendamento.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Cliente {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idCliente;
+    private int id;
     private String nome;
     private String telefone;
     private String email;
-    @Column(columnDefinition = "TEXT")
-    private String endereco;
+
+    public Cliente() {}
+
+    public Cliente(int id, String nome, String telefone, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getTelefone() { return telefone; }
+    public void setTelefone(String telefone) { this.telefone = telefone; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 }
