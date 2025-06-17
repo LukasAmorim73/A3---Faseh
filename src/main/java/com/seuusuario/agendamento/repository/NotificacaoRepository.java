47e4cbf9c16593817f982d267e38b0b26fff0ddd
LuKas-73 <amorimlukasblu@gmail.com>
2025-06-17 18:44:06 -0300

    Update NotificacaoRepository.java

--- src/main/java/com/seuusuario/agendamento/repository/NotificacaoRepository.java
+++ src/main/java/com/seuusuario/agendamento/repository/NotificacaoRepository.java
@@ -1,9 +1,25 @@
-package com.seuusuario.agendamento.repository;
+package com.seuusuario.agendamento.dao;
 
+import com.seuusuario.agendamento.ConexaoComBancoDeDados;
 import com.seuusuario.agendamento.entity.Notificacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface NotificacaoRepository extends JpaRepository<Notificacao, Long> {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class NotificacaoRepository {
+    public void cadastrarNotificacao(Notificacao notificacao) {
+        String sql = "INSERT INTO notificacao (cliente_id, mensagem) VALUES (?, ?)";
+
+        try (Connection conn = ConexaoComBancoDeDados.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, notificacao.getClienteId());
+            stmt.setString(2, notificacao.getMensagem());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
