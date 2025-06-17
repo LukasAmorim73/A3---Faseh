b660bc1f8e675522eb28f598581f78e2ee60f807
LuKas-73 <amorimlukasblu@gmail.com>
2025-06-17 18:42:51 -0300

    Update AgendamentoRepository.java

--- src/main/java/com/seuusuario/agendamento/repository/AgendamentoRepository.java
+++ src/main/java/com/seuusuario/agendamento/repository/AgendamentoRepository.java
@@ -1,9 +1,28 @@
-package com.seuusuario.agendamento.repository;
+package com.seuusuario.agendamento.dao;
 
+import com.seuusuario.agendamento.ConexaoComBancoDeDados;
 import com.seuusuario.agendamento.entity.Agendamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+public class AgendamentoRepository {
+    public void cadastrarAgendamento(Agendamento agendamento) {
+        String sql = "INSERT INTO agendamento (cliente_id, profissional_id, servico_id, data_hora) VALUES (?, ?, ?, ?)";
+
+        try (Connection conn = ConexaoComBancoDeDados.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, agendamento.getClienteId());
+            stmt.setInt(2, agendamento.getProfissionalId());
+            stmt.setInt(3, agendamento.getServicoId());
+            stmt.setTimestamp(4, Timestamp.valueOf(agendamento.getDataHora()));
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
