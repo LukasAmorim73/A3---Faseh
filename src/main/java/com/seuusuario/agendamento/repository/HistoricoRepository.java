2063c220877601f14eca594870cfd81e73c266db
LuKas-73 <amorimlukasblu@gmail.com>
2025-06-17 18:43:34 -0300

    Update HistoricoRepository.java

--- src/main/java/com/seuusuario/agendamento/repository/HistoricoRepository.java
+++ src/main/java/com/seuusuario/agendamento/repository/HistoricoRepository.java
@@ -1,9 +1,25 @@
-package com.seuusuario.agendamento.repository;
+package com.seuusuario.agendamento.dao;
 
+import com.seuusuario.agendamento.ConexaoComBancoDeDados;
 import com.seuusuario.agendamento.entity.Historico;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface HistoricoRepository extends JpaRepository<Historico, Long> {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class HistoricoRepository {
+    public void cadastrarHistorico(Historico historico) {
+        String sql = "INSERT INTO historico (cliente_id, descricao) VALUES (?, ?)";
+
+        try (Connection conn = ConexaoComBancoDeDados.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, historico.getClienteId());
+            stmt.setString(2, historico.getDescricao());
+            stmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
